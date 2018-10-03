acb8bbed875ffacb4d4c219c5b7f5df6bdc2e8da
vaheqelyan <martinkenner@mail.com>
2018-10-03 14:04:44 +0400

    Commit index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function function_name(argument) {
-	// body...
+asdfadsffunction function_name(argument) {
 	function function_name(argument) {
-		// body...}
 		function function_name(argument) {
 		// body...
 		function function_name(argument) {
 			// body...}}
 		}
+
+
+function hello(){
+	/*
+	asdasd
+	asdasdasdasdasdasdasdasd
+	asdasd
+	asdasd
+
+	asdasd
+	asdasd
+	*/
+}
