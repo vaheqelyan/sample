2bd5664db2c1f1627a84800c166ec79b54b4faea
GitHubT2 <43533755+GitHubT2@users.noreply.github.com>
2018-10-12 12:39:13 +0400

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,9 +1,1 @@
-//asdfasdfasdf
-const {execFile} = require('child_process')
-execFile("git", ["diff",'HEAD','./builder.java'], { cwd: './' }, (err,out)=>{
-	if(err){
-		console.log(err);
-		return;
-	}
-	console.log(out);
-});
+var name = 1;
