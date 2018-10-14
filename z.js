ce32de7138e1754902deac6a5eefe30454d872e0
vaheqelyan <martinkenner@mail.com>
2018-10-14 15:54:52 +0400

    asdasd

--- z.js
+++ z.js
@@ -1,3 +1,14 @@
-asd
-asd
-asd
+const {execFile,spawn}  = require('child_process');
+
+execFile('git',['commit',
+	'-m','Hello world','far.x'],{cwd:'C:/Users/Admin/Documents/sample/'},(err,out)=>{
+		if(err){
+			console.log(err);
+			return
+		}
+		console.log(out);
+	})
+
+
+// const s = spawn('git',)
+
